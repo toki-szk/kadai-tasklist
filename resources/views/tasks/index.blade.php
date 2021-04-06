72615273e74bb594bfc565b9130c14eff240319d
toki_szk <tokinoshin.suzuki@pilotz.jp>
2021-04-06 07:42:07 +0000

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+<div class="center jumbotron">
+    <div class="text-center">
+        <h1>Welcome to the タスク管理</h1>
+        {{-- ユーザ登録ページへのリンク --}}
+        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    </div>
+</div>
+
 <h1>タスク一覧</h1>
 
 @if (count($tasks) > 0)
