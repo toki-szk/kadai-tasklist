7a1e0651721b3f7f449b3a135b04a77241db5d4c
toki_szk <tokinoshin.suzuki@pilotz.jp>
2021-04-12 05:25:26 +0000

    リダイレクト設定

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -107,6 +107,8 @@ class TasksController extends Controller
             return view('tasks.edit', [
                 'task' => $task,
             ]);    
+        } else {
+            return redirect('/');
         }
     }
 
